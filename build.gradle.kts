e45f53e0775400d3783b685d2ed907846eb74144
Deaths-Door <aaravaditya51@gmail.com>
2023-05-05 09:57:41 +0200

    Try

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     kotlin("multiplatform") version "1.7.10" apply false
 
-    id("com.android.application") version "7.4.2" apply false
-    id("com.android.library") version "7.4.2" apply false
+    id("com.android.application") version "7.5" apply false
+    id("com.android.library") version "7.5" apply false
     kotlin("android") version "1.7.10"  apply false
 
     id("org.jetbrains.compose") version "1.4.0" apply false
