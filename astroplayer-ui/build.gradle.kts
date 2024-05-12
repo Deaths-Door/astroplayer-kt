67361d17b938ce16eb888191006fa001e0836640
Deaths-Door <aaravaditya51@gmail.com>
2024-05-12 17:23:44 +0200

    Revert "Publishing to MavenLocal"
    This reverts commit 14ce57751afe5dce70cd30296d85ea1ede61dc03.

--- astroplayer-ui/build.gradle.kts
+++ astroplayer-ui/build.gradle.kts
@@ -3,9 +3,6 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.documentation)
-
-    id("maven-publish")
-    id("signing")
 }
 
 kotlin {
@@ -71,60 +68,3 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 }
-
-// Based on https://medium.com/kodein-koders/publish-a-kotlin-multiplatform-library-on-maven-central-6e8a394b7030
-publishing {
-    publications {
-        repositories {
-            maven {
-                name="oss"
-
-                val repositoryId = System.getenv("SONATYPE_REPOSITORY_ID")
-                val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deployByRepositoryId/$repositoryId/")
-                val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
-
-                url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
-
-                credentials {
-                    username = System.getenv("SONATYPE_USERNAME")
-                    password = System.getenv("SONATYPE_PASSWORD")
-                }
-            }
-
-            mavenLocal()
-        }
-
-        withType<MavenPublication> {
-            pom {
-                groupId = "io.github.deathsdooor"
-                version = "0.1.1-SNAPSHORT"
-
-                name.set("astroplayer-ui")
-                description.set("AstroPlayer is an open-source media player designed for the Kotlin Multiplatform. It provides a simple API for audio playback and supports multiple media formats while also providing an Jetpack Compose UI.")
-
-                url.set("https://github.com/Deaths-Door/astroplayer-kt")
-
-                issueManagement {
-                    system.set("Github")
-                    url.set("${this@pom.url.get()}/issues")
-                }
-
-                scm {
-                    connection.set("${this@pom.url.get()}.git")
-                    url.set(this@pom.url.get())
-                }
-            }
-        }
-    }
-}
-
-// TODO : Enable this again when publishing to mavenCentral
-/*
-signing {
-    useInMemoryPgpKeys(
-        System.getenv("GPG_PRIVATE_KEY"),
-        System.getenv("GPG_PRIVATE_PASSWORD")
-    )
-
-    sign(publishing.publications)
-}*/
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,3 @@
-
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.targets.js.dsl.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.targets.js.webpack.KotlinWebpackConfig
